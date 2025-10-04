36605168dd63a5260ab18ede4f173ff5c7448b2f
Kenneth Mason <kennymmase@gmail.com>
2025-10-04 15:13:32 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kennymason.github.io/',
+  base: '/', // use '/<repo-name>/' if using github pages but not using a custom domain 
 })
